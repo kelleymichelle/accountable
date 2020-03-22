755b3404886cc201e87e70f242c4899e79a6a2a0
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-22 16:44:17 -0500

    setting up frontend. added react router and basic route pointing to home component

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>
+            Be Accountable
+          </h2>
+        </div>
+      </div>
+    )
+  }
+}
